c3fca08311598d721516f3415922e7ee572d1be7
Everton Elvio Koser <everton.elvio@gmail.com>
2015-09-12 12:32:03 -0300

    Add module AutoEquip

---
+++ AutoEquip/Source/Saveable_Outfit.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace AutoEquip
+{
+    public class Saveable_Outfit : IExposable
+    {
+        public Outfit outfit;
+        public List<Saveable_Outfit_StatDef> stats = new List<Saveable_Outfit_StatDef>();
+
+        public void ExposeData()
+        {
+            Scribe_References.LookReference(ref this.outfit, "outfit");
+            Scribe_Collections.LookList(ref this.stats, "stats", LookMode.Deep);
+        }
+    }
+}
---
+++ AutoEquipInfusion/Source/MapComponent_AutoEquipInfusion.cs
@@ -0,0 +1,38 @@
+﻿using AutoEquip;
+using Infusion;
+using RimWorld;
+using Verse;
+
+namespace AutoEquipInfusion
+{
+    public class MapComponent_AutoEquipInfusion : MapComponent
+    {
+        static MapComponent_AutoEquipInfusion()
+        {
+            Log.Message("AutoEquip with Infusion Initialized");
+            JobGiver_OptimizeApparelAutoEquip.ApparelScoreRawStatsHandlers += JobGiver_OptimizeApparelAutoEquip_ApparelScoreRawStatsHandlers;
+        }
+
+        static void JobGiver_OptimizeApparelAutoEquip_ApparelScoreRawStatsHandlers(Pawn pawn, Apparel apparel, StatDef stat, ref float val)
+        {
+            InfusionSet inf;
+            if (apparel.TryGetInfusions(out inf))
+            {               
+                StatMod mod;                
+                var prefix = inf.Prefix.ToInfusionDef();
+                var suffix = inf.Suffix.ToInfusionDef();
+
+                if (!inf.PassPre && prefix.GetStatValue(stat, out mod))
+                {
+                    val += mod.offset;
+                    val *= mod.multiplier;
+                }
+                if (inf.PassSuf || !suffix.GetStatValue(stat, out mod))
+                    return;
+
+                val += mod.offset;
+                val *= mod.multiplier;
+            }
+        }
+    }
+}
